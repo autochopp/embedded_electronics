b7a493b5ddc64847df7e4f1708f95e72310f0cd5
ithallojunior <ithallojunior@outlook.com>
2017-11-10 20:56:47 +0000

    making a functional code to control the temperature

--- temperature_sensor/temp_control.py
+++ temperature_sensor/temp_control.py
@@ -4,29 +4,30 @@ from temperature_reader import getTemperature
 import time
 import  RPi.GPIO as gpio
 
-def temp_control(max_temp=2., active_low=False, interval=2.):
+def temp_control(min_temp=7., max_temp=10., active_low=False, interval=2.):
 	relay_pin = 21
 	gpio.setmode(gpio.BCM)
 	gpio.setup(relay_pin, gpio.OUT)
-
-        if active_low:
+  	
+	if active_low:
 		active = 0
 		inactive = 1
 	else:
 		active = 1
 		inactive = 0
 
-        temp = getTemperature()
 
+	temp = getTemperature()
 	if (temp>max_temp):
 		gpio.output(relay_pin, active)
-		print "relay on! temperature:", temp
-
-	else:
+		print "relay on! "
+	
+	if(temp<min_temp):
 		gpio.output(relay_pin, inactive)
-		print "relay off! temperature:", temp
-
-        time.sleep(interval)
+		print "relay off!"
+	print "temperature", temp
+	
+	time.sleep(interval)
 
 if __name__ =="__main__":
 	while True:
