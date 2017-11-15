6c44746e37b3d357a01b075b2f6bafa3067f8ed1
ithallojunior <ithallojunior@outlook.com>
2017-11-15 21:18:00 +0000

    placeholder for the software people

---
+++ machine_controller.py
@@ -0,0 +1,14 @@
+class MachineController:
+
+	def __init__():
+		pass
+
+	#returns true if the cup is placed and in the right position to get beer
+	def cup_activate(self):
+		pass
+	
+	# returns true when the beer was already taken
+	def already_got_beer(self):
+		pass
+
+
