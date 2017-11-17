02417e814ad7a5457a2f189b00dc81bf0a015d9d
ithallojunior <ithallojunior@outlook.com>
2017-11-17 18:17:59 +0000

    adding missing function

--- machine_controller.py
+++ machine_controller.py
@@ -2,7 +2,10 @@ class MachineController:
 
 	def __init__():
 		pass
-
+	
+	#returns true when the cup drawer is open
+	def is_drawer_open(self):
+		pass
 	#returns true if the cup is placed and in the right position to get beer
 	def cup_activate(self):
 		pass
