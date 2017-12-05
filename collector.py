3885a51822a630cc9a4c16659c20bfc6c696a17c
ithallojunior <ithallojunior@outlook.com>
2017-12-05 00:02:45 -0200

    adding full path, lazy way to fix it

--- collector.py
+++ collector.py
@@ -5,48 +5,53 @@ from temperature  import getTemperature
 class Collector:
 
     def __init__(self, server_url='http://127.0.0.1', port="3000"):
-		self.temperature = 0.0
-		self.volume = 0 # in ml, int
-		self.machine_status = False
-		self.sent_status = False
-		# Rails API is running in localhost:3000
-		self.server_url = server_url + ':' + str(port) + '/setsensors'
+        self.temperature = 0.0
+        self.volume = 0 # in ml, int
+        self.machine_status = False
+        self.sent_status = False
+        # Rails API is running in localhost:3000
+        self.server_url = server_url + ':' + str(port) + '/setsensors'
 
     # Collect all sensors data
     def collect(self):
-		self.temperature = getTemperature()
-		f = open("volume.vol", "r")
-		self.volume = int(f.read())
-		f.close()
-		
+        try:
+            self.temperature = getTemperature()
+        except:
+            self.temperature = 'null'
+        try:
+            f = open("/home/pi/autochopp-machine/embedded_electronics/volume.vol", "r")
+            self.volume = int(f.read())
+            f.close()
+        except:
+            self.volume = 'null'
 
     # Send the HTTP POST request
     def send_to_server(self):
-		data = {
-			'temperature': self.temperature,
-			'volume': self.volume,
-			'machine_status': self.machine_status
-			}
-		r = requests.post(self.server_url, data)
-		if r.status_code == 204:
-			self.sent_status = True
-		else:
-			self.sent_status = False	
+        data = {
+            'temperature': self.temperature,
+            'volume': self.volume,
+            'machine_status': self.machine_status
+            }
+        r = requests.post(self.server_url, data)
+        if r.status_code == 204:
+            self.sent_status = True
+        else:
+            self.sent_status = False    
 
 
 ####################### TEST ###################
 if __name__=="__main__":
-	test = Collector("https://fast-retreat-18030.herokuapp.com", '')
-	import time
-	
-	while(1):
-		try:
-			test.collect()
-			print "\nTemperature: ",  test.temperature
-			print "Volume: ", test.volume
-			test.send_to_server()
-			print "Sent status: ", test.sent_status
-			time.sleep(5)
-		except KeyboardInterrupt:
-			break		
+    test = Collector("https://fast-retreat-18030.herokuapp.com", '')
+    import time
+    
+    while(1):
+        try:
+            test.collect()
+            print "\nTemperature: ",  test.temperature
+            print "Volume: ", test.volume
+            test.send_to_server()
+            print "Sent status: ", test.sent_status
+            time.sleep(60)
+        except KeyboardInterrupt:
+            break        
 
